8b0f90fbc81a9380910b67e753038b4edad9b4ff
iSlesa <artycrab_slesh@yahoo.com>
2016-06-24 00:17:06 +0545

    List all, add color, add indentation

---
+++ tree.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+import termcolor
+
+def listall(path,j=0):
+    list_ = os.listdir(path)
+    os.chdir(path)
+    num = len(list_)
+    for i in range(num):
+        if not list_[i].startswith('.'):
+            if os.path.isdir(list_[i]):
+                if i == (num)-1:
+                    print("│    "*j+"└──",end=" ")
+                    termcolor.cprint(list_[i], 'green',attrs=['bold'] )
+                else:
+                    print("│    "*j+'├──',end=" ")
+                    termcolor.cprint(list_[i], 'green',attrs=['bold'] )
+                path_ = path+'/'+list_[i]
+                os.chdir(path_)
+                listall(path_,j+1)
+            else:
+                if i == (num)-1:
+                    print("│    "*j+'└── '+list_[i])
+                else:
+                    print("│    "*j+'├── '+list_[i])
+        os.chdir(path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, default=os.getcwd(), help="Path to directory")
+    path = os.path.abspath(parser.parse_args().path)
+    if not os.path.isdir(path):
+        print(path+" is not a directory. Please enter a valid path.")
+    else:
+        termcolor.cprint(os.path.basename(os.path.normpath(path)), 'blue', attrs=['bold'])
+        listall(path) 
